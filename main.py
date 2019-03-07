cbddc7216e58a0cb2cdc38787c1152e0a7c4d906
NPM8 <nnnp.dawid@gmail.com>
2019-03-08 00:59:55 +0100

    Done
    All staff with main code done but doc comments

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 import os
+import copy
 import sys
 from typing import List, Tuple
 
@@ -22,43 +24,42 @@ def alfarray(alphabet="0123456789abcdefghijklmnopqrstwvuxyzABCDEFGHIJKLMNOPQRSTW
     return sorted([[val[0], val[1] / signgs] for val in toret if val[1] != 0], key=lambda v: v[1])
 
 
-def genArray(alphabet="0123456789abcdefghijklmnopqrstwvuxyzABCDEFGHIJKLMNOPQRSTWVUXYZ,. \t\n", file="file.txt"):
+def genArrayOfCompartments(alphabet="0123456789abcdefghijklmnopqrstwvuxyzABCDEFGHIJKLMNOPQRSTWVUXYZ,. \t\n", file="file.txt"):
     tmp_array_of_compartments: List[Compartment] = []
-
     for i, val in enumerate(alfarray(alphabet, file)):
         tmp_elem = 0.0 if i == 0 else tmp_array_of_compartments[i - 1].range[1]
         tmp_array_of_compartments.append(Compartment(
             (tmp_elem, tmp_elem + val[1]),
             val[0]
         ))
-
-    print(tmp_array_of_compartments)
     return tmp_array_of_compartments
 
 
 def encode(alphabet: str = "0123456789abcdefghijklmnopqrstwvuxyzABCDEFGHIJKLMNOPQRSTWVUXYZ,. \t\n",
            file: str = "file.txt", output: str = "out_file.txt") -> None:
-    array_of_compartments = genArray(alphabet, file)
+    array_of_compartments = genArrayOfCompartments(alphabet, file)
     if os.path.exists(output):
         os.remove(output)
     with open(file, "r") as input_file:
         for line in input_file.readlines():
             tmp_str = ''
-            print(line)
             for elem in line.split():
-                print(line.split())
                 number = 0.0
-                tmp_array = array_of_compartments
+                tmp_array = copy.deepcopy(array_of_compartments)
                 for sign in elem:
                     compartment = next(x for x in tmp_array if x.letter == sign)
                     first = compartment.range[0]
                     last = compartment.range[1]
                     size = last - first
+                    # print('Litera {} zakres: {}, {} wielkość: {}'.format(compartment.letter, compartment.range[0],
+                    #                                                      compartment.range[1], size))
                     for itr, val in enumerate(array_of_compartments):
-                        value1: float = tmp_array[itr - 1].range[1] if itr != 0 else first
-                        value2: float = value1 + size * val.range[1] if itr != len(array_of_compartments) else last
-                        tmp_array[itr].set_range((value1, value2))
-                number = (tmp_array[0].range[0] + tmp_array[-1].range[1]) / 2
+                        value1: float = first + (size * val.range[0])
+                        value2: float = first + (size * val.range[1])
+                        # print('Value: {}, {} array val: {}, {}'.format(value1, value2, val.range[0], val.range[1]))
+                        # tmp_array[itr].set_range((value1, value2))
+                print(tmp_array[0], tmp_array[-1])
+                number = math.fsum([tmp_array[0].range[0], tmp_array[-1].range[1]]) / 2
                 with open(output, "a+") as output_file:
                     output_file.write(f'{number} {len(elem)}#')
             with open(output, "a+") as output_file:
@@ -66,16 +67,34 @@ def encode(alphabet: str = "0123456789abcdefghijklmnopqrstwvuxyzABCDEFGHIJKLMNOP
 
 
 def decode(array_of_compartment,
-           file: str = "file.txt", output: str = "out_file.txt") -> None:
+           file: str = "out_file.txt", output: str = "decodet_out_file.txt") -> None:
+    if os.path.exists(output): os.remove(output)
     with open(file, "r") as input_file:
         for line in input_file.readlines():
             for numbers in line.split("#"):
-                point, number = numbers.split()[0], numbers.split()[1]
-                tmp_array = array_of_compartment
-                for i in range(1, number):
-                    tmp_array
-
-    return None
+                if numbers == '\n':
+                    continue
+                point: float = float(numbers.split()[0])
+                number: int = int(numbers.split()[1])
+                tmp_array: List[Compartment] = copy.deepcopy(array_of_compartment)
+                for i in range(number):
+                    compartment = next(x for x in tmp_array if x.range[0] <= point <= x.range[1])
+                    first = compartment.range[0]
+                    last = compartment.range[1]
+                    size = last - first
+                    for itr, val in enumerate(array_of_compartment):
+                        value1: float = first + (size * val.range[0])
+                        value2: float = first + (size * val.range[1])
+                        # print('Value: {}, {} array val: {}, {}'.format(value1, value2, val.range[0], val.range[1]))
+                        tmp_array[itr].set_range((value1, value2))
+                    with open(output, 'a+') as output_file:
+                        output_file.write(compartment.letter)
+                with open(output, 'a+') as output_file:
+                    output_file.write(" ")
+            with open(output, 'a+') as output_file:
+                output_file.write('\n')
 
 
 encode()
+
+decode(genArrayOfCompartments())
